ecce75676e81bd53e060486954f3854f1b462c11
Aime <stecklineblane@gmail.com>
2023-04-17 23:10:03 -0700

    more progress on spark card

--- src/components/GenerateSparkCard.tsx
+++ src/components/GenerateSparkCard.tsx
@@ -1,7 +1,14 @@
+import { createBasicSparkPrompt } from "@/utils/contentUtils";
+import { User, UserProfile } from "firebase/auth";
 import React from "react";
 import { useState } from "react";
 
-const GenerateSparkCard: React.FC = () => {
+interface GenerateSparkCardProps {
+  user: User | null;
+  userProfile: UserProfile;
+}
+
+const GenerateSparkCard: React.FC = (user, userProfile) => {
   const themes = [
     "Inspirational",
     "Educational",
@@ -40,6 +47,35 @@ const GenerateSparkCard: React.FC = () => {
       );
     }
   };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!selectedKeywords[0]) {
+      alert("Please fill in the required fields.");
+      return;
+    }
+    // Prepare the OpenAI API parameters
+    const prompt = createBasicSparkPrompt(
+      userProfile,
+      selectedTheme,
+      selectedKeywords
+    );
+    const options = {
+      message: prompt,
+      context:
+        "You are a professional content creator assistant here to help anyone grow their business or brand.",
+      maxTokens: 100,
+      n: 1,
+      temperature: 0.7,
+    };
+  };
+  // const handleSave = () => {
+  //   const contentCategory = selectedPlatform.toLowerCase().concat(contentType);
+  //   if (user) {
+  //     saveGeneratedContent(user?.uid, editedContent, contentCategory);
+  //   }
+  //   setShowModal(false);
+  // };
 
   return (
     <div className="w-[100%] h-full card bg-base-300 mx-auto p-10 shadow-xl">
@@ -88,6 +124,7 @@ const GenerateSparkCard: React.FC = () => {
           className="btn btn-primary btn-sm m-2"
           onClick={async () => {
             setSparkingIdea(true);
+            handleSubmit;
             // await generate prompt
             setTimeout(() => {
               setSparkingIdea(false);
@@ -96,131 +133,6 @@ const GenerateSparkCard: React.FC = () => {
         >
           {sparkingIdea ? "Sparking..." : "Spark Ideas!"}
         </button>
-        {/* <form onSubmit={handleSubmit} className="w-full max-w-sm mx-auto">
-          <label htmlFor="platform" className="text-md">
-            Platform:
-          </label>
-          <div className="flex flex-wrap justify-evenly p-4 max-w-md">
-            {["Twitter", "LinkedIn"].map((platform) => (
-              <div key={platform} className="form-control">
-                <label className="label cursor-pointer">
-                  <span className="label-text px-2">{platform}</span>
-                  <input
-                    type="radio"
-                    name="radio-10"
-                    className={`radio checked:bg-primary `}
-                    value={platform}
-                    onChange={(e) => setSelectedPlatform(e.target.value)}
-                    checked={selectedPlatform === platform}
-                  />
-                </label>
-              </div>
-            ))}
-          </div>
-
-          <label htmlFor="tone" className="text-md">
-            Choose a tone:
-          </label>
-          <div className="grid grid-cols-2 justify-evenly p-4 max-w-md">
-            {[
-              "Professional",
-              "Friendly",
-              "Casual",
-              "Humorous",
-              "Inspirational",
-            ].map((tone) => (
-              <div
-                key={tone}
-                className="form-control hover:outline outline-2 outline-primary m-1 rounded-md ease-in-out-[0.2s]"
-              >
-                <label className="label cursor-pointer">
-                  <span className="label-text px-2">{tone}</span>
-                  <input
-                    type="radio"
-                    name="radio-tone"
-                    className={`radio checked:bg-primary`}
-                    value={tone}
-                    onChange={(e) => setSelectedTone(e.target.value)}
-                    checked={selectedTone === tone}
-                  />
-                </label>
-              </div>
-            ))}
-          </div>
-
-          <label htmlFor="content-type" className="text-md">
-            Content Type:
-          </label>
-          <div className="grid grid-cols-2 p-4 max-w-md">
-            {renderContentTypeOptions(selectedPlatform)}
-          </div>
-
-          {contentType === "thread" && (
-            <div className="w-full flex flex-col items-center">
-              <label htmlFor="thread-length" className="text-md">
-                Thread Length:
-              </label>
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (threadLength > 2) {
-                      setThreadLength(threadLength - 1);
-                    }
-                  }}
-                  className="btn btn-primary"
-                >
-                  -
-                </button>
-                <input
-                  type=""
-                  id="thread-length"
-                  value={threadLength}
-                  className="input input-bordered w-20 mx-2 text-center"
-                  min="2"
-                  max="10"
-                  onChange={(e) => {
-                    const value = parseInt(e.target.value);
-                    if (value >= 2 && value <= 10) {
-                      setThreadLength(value);
-                    }
-                  }}
-                />
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (threadLength < 10) {
-                      setThreadLength(threadLength + 1);
-                    }
-                  }}
-                  className="btn btn-primary"
-                >
-                  +
-                </button>
-              </div>
-            </div>
-          )}
-          {platformContentSelectedOptions[contentType]?.inputs.map((input) => (
-            <div key={input.id} className="flex flex-col p-4">
-              <label htmlFor={input.id} className="text-md">
-                {input.label}
-              </label>
-              <textarea
-                id={input.id}
-                className="textarea textarea-bordered w-full"
-                onChange={(e) => input.stateUpdater(e.target.value)}
-                placeholder={input.placeholder}
-              />
-            </div>
-          ))}
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="btn btn-primary m-4"
-          >
-            Generate
-          </button>
-        </form> */}
       </div>
     </div>
   );
